fcee6c0701d051ee66b577df1fc8c5f263b7edb7
Carles Sala <c.sala.stq@gmail.com>
2019-09-18 20:33:54 +0200

    better templates, soft-timeout, label encoding

--- autobazaar/__main__.py
+++ autobazaar/__main__.py
@@ -4,6 +4,7 @@
 """AutoBazaar Command Line Module."""
 
 import argparse
+import gc
 import json
 import os
 import shutil
@@ -23,7 +24,7 @@ from mit_d3m.utils import logging_setup, make_abs
 
 import autobazaar
 from autobazaar.search import TUNERS, PipelineSearcher
-from autobazaar.utils import make_keras_picklable
+from autobazaar.utils import make_keras_picklable, encode_score
 
 warnings.filterwarnings(action='ignore')
 
@@ -123,7 +124,7 @@ def _score_predictions(dataset, problem, predictions, input_dir):
 
     targets = targets.iloc[:, 0]
     predictions = predictions.iloc[:, 0]
-    score = metric(targets, predictions)
+    score = encode_score(metric, targets, predictions)
     print("Score: {}".format(score))
 
     summary = {'predictions': predictions, 'targets': targets}
@@ -148,7 +149,8 @@ def _box_print(message):
     print(length * '#')
 
 
-def _insert_test(args, dataset, start_ts):
+def _insert_test(args, dataset):
+    insert_ts = datetime.utcnow()
     document = {
         'test_id': args.test_id,
         'dataset': dataset,
@@ -157,8 +159,8 @@ def _insert_test(args, dataset, start_ts):
         'budget': args.budget,
         'template': args.template,
         'status': 'running',
-        'insert_ts': start_ts,
-        'update_ts': start_ts,
+        'insert_ts': insert_ts,
+        'update_ts': insert_ts,
         'version': VERSION,
         'hostname': socket.gethostname(),
         'tuner_type': args.tuner_type,
@@ -193,7 +195,7 @@ def _insert_test_result(args, result):
 def _score_dataset(dataset, args):
     start_ts = datetime.utcnow()
     if args.db:
-        _insert_test(args, dataset, start_ts)
+        _insert_test(args, dataset)
 
     result_base = {
         'dataset': dataset,
@@ -204,7 +206,6 @@ def _score_dataset(dataset, args):
         'step': None,
         'load_time': None,
         'trivial_time': None,
-        'fit_time': None,
         'cv_time': None,
         'cv_score': None,
         'rank': None
@@ -224,6 +225,7 @@ def _score_dataset(dataset, args):
             dataset, args.problem, args.template, args.input, args.output, args.budget,
             args.checkpoints, args.splits, args.db, args.tuner_type, args.test_id
         )
+        gc.collect()
 
         for search_result in search_results or []:
             result = result_base.copy()
@@ -242,6 +244,7 @@ def _score_dataset(dataset, args):
                 score = _score_predictions(dataset, args.problem,
                                            predictions, args.input)
                 result['score'] = score
+                gc.collect()
 
             except Exception as e:
                 error = _format_exception(e)
@@ -384,7 +387,6 @@ REPORT_COLUMNS = [
     'iterations',
     'load_time',
     'trivial_time',
-    'fit_time',
     'cv_time',
     'error',
     'step'
@@ -399,7 +401,11 @@ def _list(args):
         'data_modality', 'task_type', 'task_subtype', 'metric', 'size_human', 'train_samples'
     ]
     datasets = datasets.reindex(columns, axis=1)
-    print(datasets.to_string(columns=columns, index=True))
+    if args.report:
+        print("Storing datasets as {}".format(args.report))
+        datasets[columns].to_csv(args.report, index=True)
+    else:
+        print(datasets.to_string(columns=columns, index=True))
 
 
 class ArgumentParser(argparse.ArgumentParser):
@@ -494,7 +500,7 @@ def _get_parser():
     subparsers = parser.add_subparsers(title='command', help='Command to execute')
     parser.set_defaults(command=None)
 
-    list_ = subparsers.add_parser('list', parents=[logging_args, dataset_args],
+    list_ = subparsers.add_parser('list', parents=[logging_args, dataset_args, report_args],
                                   help='List the available datasets that match the conditions.')
     list_.set_defaults(command=_list)
 
@@ -521,6 +527,7 @@ def main():
         parser.exit()
 
     logging_setup(args.verbose, args.logfile)
+    gc.enable()
 
     args.command(args)
 
--- autobazaar/search.py
+++ autobazaar/search.py
@@ -7,6 +7,7 @@ This module contains the PipelineSearcher, which is the class that
 contains the main logic of the Auto Machine Learning process.
 """
 
+import gc
 import itertools
 import json
 import logging
@@ -14,9 +15,10 @@ import os
 import signal
 import warnings
 from collections import defaultdict
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import numpy as np
+import pandas as pd
 from btb import HyperParameter
 from btb.tuning import GP, GPEi, Uniform
 from mit_d3m.loaders import get_loader
@@ -31,7 +33,6 @@ LOGGER = logging.getLogger(__name__)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 TEMPLATES_DIR = os.path.join(os.path.dirname(__file__), 'templates')
-
 
 TRIVIAL_PIPELINE_METHOD = {
     'classification': 'mode',
@@ -51,12 +52,37 @@ PARAM_TYPES = {
 }
 
 
-class StopSearch(Exception):
+class StopSearch(KeyboardInterrupt):
     pass
 
 
 class UnsupportedProblem(Exception):
     pass
+
+
+def log_times(name, append=False):
+    def decorator(wrapped):
+        def wrapper(self, *args, **kwargs):
+            start = datetime.utcnow()
+            result = wrapped(self, *args, **kwargs)
+            elapsed = (datetime.utcnow() - start).total_seconds()
+
+            if append:
+                attribute = getattr(self, name, None)
+                if attribute is None:
+                    attribute = list()
+                    setattr(self, name, attribute)
+
+                attribute.append(elapsed)
+
+            else:
+                setattr(self, name, elapsed)
+
+            return result
+
+        return wrapper
+
+    return decorator
 
 
 class PipelineSearcher(object):
@@ -82,33 +108,41 @@ class PipelineSearcher(object):
 
         self._test_id = test_id
 
-    def _dump(self, pipeline):
-        if not pipeline.dumped:
-            pipeline.fit(self.data_params)
+    def _dump_pipelines(self):
+        LOGGER.info('Dumping best pipelines')
+        dumped = list()
+        gc.collect()
+        for details in self._to_dump:
+            pipeline = details['pipeline']
+            if not pipeline.dumped:
+                pipeline.fit(self.data_params)
 
-            mlpipeline = pipeline.pipeline
+                mlpipeline = pipeline.pipeline
 
-            LOGGER.info("Dumping pipeline %s: %s", pipeline.id, pipeline.pipeline)
-            LOGGER.info("Hyperparameters: %s", mlpipeline.get_hyperparameters())
-            pipeline.dump(self._pipelines_dir)
+                LOGGER.info("Dumping pipeline %s: %s", pipeline.id, pipeline.pipeline)
+                LOGGER.info("Hyperparameters: %s", mlpipeline.get_hyperparameters())
 
-        else:
-            LOGGER.info("Skipping already dumped pipeline %s", pipeline.id)
+                pipeline.dump(self._pipelines_dir)
 
-        self.dumped.append({
+                details['pipeline'] = pipeline.id
+                dumped.append(details)
+                gc.collect()
+
+            else:
+                LOGGER.info("Skipping already dumped pipeline %s", pipeline.id)
+
+        return dumped
+
+    def _set_for_dump(self, pipeline):
+        self._to_dump.append({
             'elapsed': (datetime.utcnow() - self.start_time).total_seconds(),
             'iterations': len(self.pipelines) - 1,
             'cv_score': self.best_pipeline.score,
             'rank': self.best_pipeline.rank,
-            'pipeline': self.best_pipeline.id,
+            'pipeline': pipeline,
             'load_time': self.load_time,
             'trivial_time': self.trivial_time,
-            'fit_time': self.fit_time,
-            'cv_time': self.cv_time,
-            'data_modality': self.data_modality,
-            'task_type': self.task_type,
-            'task_subtype': self.task_subtype,
-            'metric': self.metric
+            'cv_time': np.sum(self.cv_times),
         })
 
     def _save_pipeline(self, pipeline):
@@ -126,6 +160,7 @@ class PipelineSearcher(object):
             insertable['test_id'] = self._test_id
             self._db.pipelines.insert_one(insertable)
 
+    @log_times('trivial_time')
     def _build_trivial_pipeline(self):
         LOGGER.info("Building the Trivial pipeline")
         try:
@@ -194,6 +229,8 @@ class PipelineSearcher(object):
             if not template:
                 raise ValueError("Template {} not found".format(template_name))
 
+            primitives = '\n'.join(template['primitives'])
+            LOGGER.info('Using template %s:\n%s', template_name, primitives)
             return template
 
         else:
@@ -211,6 +248,8 @@ class PipelineSearcher(object):
                 template_name = '/'.join(problem_type[:levels] + ['default'])
                 template = self._load_template(template_name)
                 if template:
+                    primitives = '\n'.join(template['primitives'])
+                    LOGGER.info('Using template %s:\n%s', template_name, primitives)
                     return template
 
             # Nothing has been found for this modality/task/subtask combination
@@ -218,40 +257,36 @@ class PipelineSearcher(object):
             LOGGER.error('Problem type not supported %s', problem_type)
             raise UnsupportedProblem(problem_type)
 
-    def _build_default_pipeline(self, template_dict):
-        LOGGER.info("Building the default pipeline")
-        pipeline = ABPipeline(template_dict, self.loader, self.metric, self.problem_doc)
+    @log_times('cv_times', append=True)
+    def _cv_pipeline(self, params=None):
+        pipeline_dict = self.template_dict.copy()
+        if params:
+            pipeline_dict['hyperparameters'] = params
+
+        pipeline = ABPipeline(pipeline_dict, self.loader, self.metric, self.problem_doc)
 
         X = self.data_params.X
         y = self.data_params.y
         context = self.data_params.context
 
-        X = pipeline.preprocess(X, y, context)
-
         try:
             pipeline.cv_score(X, y, context, cv=self.kf)
-
-            LOGGER.info("Saving the default pipeline %s", pipeline.id)
-
-            self._save_pipeline(pipeline)
-        except StopSearch:
+        except KeyboardInterrupt:
             raise
-
         except Exception:
-            # if the Default pipeline crashes we can do nothing,
-            # so we just log the error and move on.
-            LOGGER.exception("The Default pipeline crashed.")
-            pipeline = None
+            LOGGER.exception("Crash cross validating pipeline %s", pipeline.id)
+            return None
 
-        return pipeline, X, y, context
+        return pipeline
 
-    def _get_tuner(self, pipeline, template_dict):
+    def _create_tuner(self, pipeline):
         # Build an MLPipeline to get the tunables and the default params
-        mlpipeline = MLPipeline.from_dict(template_dict)
+        mlpipeline = MLPipeline.from_dict(self.template_dict)
+        tunable_hyperparameters = mlpipeline.get_tunable_hyperparameters()
 
         tunables = []
         tunable_keys = []
-        for block_name, params in mlpipeline.get_tunable_hyperparameters().items():
+        for block_name, params in tunable_hyperparameters.items():
             for param_name, param_details in params.items():
                 key = (block_name, param_name)
                 param_type = param_details['type']
@@ -267,7 +302,8 @@ class PipelineSearcher(object):
 
         # Create the tuner
         LOGGER.info('Creating %s tuner', self._tuner_class.__name__)
-        tuner = self._tuner_class(tunables)
+
+        self.tuner = self._tuner_class(tunables)
 
         if pipeline:
             try:
@@ -282,16 +318,18 @@ class PipelineSearcher(object):
 
                             default_params[key] = value
 
-                tuner.add(default_params, 1 - pipeline.rank)
+                if pipeline.rank is not None:
+                    self.tuner.add(default_params, 1 - pipeline.rank)
+
             except ValueError:
                 pass
-
-        return tuner
 
     def _set_checkpoint(self):
         next_checkpoint = self.checkpoints.pop(0)
         interval = next_checkpoint - self.current_checkpoint
-        LOGGER.info("Setting %s seconds checkpoint in %s seconds", next_checkpoint, interval)
+        self._stop_time = datetime.utcnow() + timedelta(seconds=interval)
+        LOGGER.info("Setting %s seconds checkpoint in %s seconds: %s",
+                    next_checkpoint, interval, self._stop_time)
         signal.alarm(interval)
         self.current_checkpoint = next_checkpoint
 
@@ -304,22 +342,31 @@ class PipelineSearcher(object):
 
         try:
             if self.best_pipeline:
-                self._dump(self.best_pipeline)
+                self._set_for_dump(self.best_pipeline)
 
-        except StopSearch:
+        except KeyboardInterrupt:
             raise
         except Exception:
             LOGGER.exception("Checkpoint dump crashed")
 
         if final or not bool(self.checkpoints):
             self.current_checkpoint = None
-            raise StopSearch()
+            # LOGGER.warn("Stopping Search")
+            # raise StopSearch()
         else:
             self._set_checkpoint()
 
-    def search(self, d3mds, template_name=None, budget=None, checkpoints=None):
+    def _check_stop(self):
+        if self._stop_time and self._stop_time < datetime.utcnow():
+            LOGGER.warn("Stop Time already passed. Stopping Search!")
+            raise StopSearch()
+
+    def _setup_search(self, d3mds, budget, checkpoints, template_name):
+        self.start_time = datetime.utcnow()
+        self.cv_times = list()
+
         # Problem variables
-        problem_id = d3mds.get_problem_id()
+        self.problem_id = d3mds.get_problem_id()
 
         self.task_type = d3mds.get_task_type()
         self.task_subtype = d3mds.problem.get_task_subtype()
@@ -347,114 +394,95 @@ class PipelineSearcher(object):
         self.pipelines = []
         self.checkpoints = sorted(checkpoints or [])
         self.current_checkpoint = 0
-        self.dumped = []
+        self._to_dump = []
 
         if not self.checkpoints and budget is None:
-            budget = 1
+            self.budget = 1
+        else:
+            self.budget = budget
 
-        self.load_time = None
-        self.trivial_time = None
-        self.fit_time = None
-        self.cv_times = []
-        self.cv_time = None
+        self.template_dict = self._get_template(template_name)
 
-        try:
-            self.start_time = datetime.utcnow()
+        LOGGER.info("Running TA2 Search")
+        LOGGER.info("Problem Id: %s", self.problem_id)
+        LOGGER.info("    Data Modality: %s", self.data_modality)
+        LOGGER.info("    Task type: %s", self.task_type)
+        LOGGER.info("    Task subtype: %s", self.task_subtype)
+        LOGGER.info("    Metric: %s", self.metric)
+        LOGGER.info("    Checkpoints: %s", self.checkpoints)
+        LOGGER.info("    Budget: %s", self.budget)
 
-            LOGGER.info("Running TA2 Search")
-            LOGGER.info("Problem Id: %s", problem_id)
-            LOGGER.info("    Data Modality: %s", self.data_modality)
-            LOGGER.info("    Task type: %s", self.task_type)
-            LOGGER.info("    Task subtype: %s", self.task_subtype)
-            LOGGER.info("    Metric: %s", self.metric)
-            LOGGER.info("    Checkpoints: %s", self.checkpoints)
-            LOGGER.info("    Budget: %s", budget)
+    @log_times('load_time')
+    def _load_data(self, d3mds):
+        self.data_params = self.loader.load(d3mds)
 
-            if self.checkpoints:
-                signal.signal(signal.SIGALRM, self._checkpoint)
-                self._set_checkpoint()
-
-            load_start = datetime.utcnow()
-            self.data_params = self.loader.load(d3mds)
-            load_end = datetime.utcnow()
-            self.load_time = (load_end - load_start).total_seconds()
-
+    def _setup_cv(self):
+        if isinstance(self.data_params.y, pd.Series):
             min_samples = self.data_params.y.value_counts().min()
-            if self.task_type == 'classification' and min_samples >= self._cv_splits:
-                self.kf = StratifiedKFold(
-                    n_splits=self._cv_splits,
-                    shuffle=True,
-                    random_state=self._random_state
-                )
-            else:
-                self.kf = KFold(
-                    n_splits=self._cv_splits,
-                    shuffle=True,
-                    random_state=self._random_state
-                )
+        else:
+            y = self.data_params.y
+            min_samples = y.groupby(list(y.columns)).size().min()
+
+        if self.task_type == 'classification' and min_samples >= self._cv_splits:
+            self.kf = StratifiedKFold(
+                n_splits=self._cv_splits,
+                shuffle=True,
+                random_state=self._random_state
+            )
+        else:
+            self.kf = KFold(
+                n_splits=self._cv_splits,
+                shuffle=True,
+                random_state=self._random_state
+            )
+
+    def search(self, d3mds, template_name=None, budget=None, checkpoints=None):
+        try:
+            self._setup_search(d3mds, budget, checkpoints, template_name)
+            self._load_data(d3mds)
+            self._setup_cv()
 
             # Build the trivial pipeline
-            trivial_start = datetime.utcnow()
             self.best_pipeline = self._build_trivial_pipeline()
-            trivial_end = datetime.utcnow()
-            self.trivial_time = (trivial_end - trivial_start).total_seconds()
-
-            template_dict = self._get_template(template_name)
 
             # Do not continue if there is no budget or no fit data
             if budget == 0 or not len(self.data_params.X):
                 raise StopSearch()
 
             # Build the default pipeline
-            fit_start = datetime.utcnow()
-            default_pipeline, X, y, context = self._build_default_pipeline(template_dict)
+            default_pipeline = self._cv_pipeline()
             if default_pipeline:
                 self.best_pipeline = default_pipeline
-
-            fit_end = datetime.utcnow()
-            self.fit_time = (fit_end - fit_start).total_seconds()
+                self._save_pipeline(default_pipeline)
 
             if budget == 1:
                 raise StopSearch()
             elif budget is not None:
-                budget -= 1
-
-            # Build the tuner
-            tuner = self._get_tuner(default_pipeline, template_dict)
-
-            LOGGER.info("Starting the tuning loop")
-
-            if budget is not None:
-                iterator = range(budget)
+                iterator = range(budget - 1)
             else:
                 iterator = itertools.count()   # infinite range
 
+            # Build the tuner
+            self._create_tuner(default_pipeline)
+
+            LOGGER.info("Starting the tuning loop")
+
+            if self.checkpoints:
+                signal.signal(signal.SIGALRM, self._checkpoint)
+                self._set_checkpoint()
+            else:
+                self._stop_time = None
+
             for iteration in iterator:
-                proposed_params = tuner.propose()
+                self._check_stop()
+                proposed_params = self.tuner.propose()
                 params = make_dumpable(proposed_params)
 
-                cv_start = datetime.utcnow()
-                pipeline_dict = template_dict.copy()
-                pipeline_dict['hyperparameters'] = params
-                pipeline = ABPipeline(pipeline_dict, self.loader,
-                                      self.metric, self.problem_doc)
-                try:
-                    LOGGER.info("Cross validating pipeline %s", iteration + 1)
-                    pipeline.cv_score(X, y, context, cv=self.kf)
-                except StopSearch:
-                    raise
+                LOGGER.info("Cross validating pipeline %s", iteration + 1)
+                pipeline = self._cv_pipeline(params)
 
-                except Exception:
-                    LOGGER.exception("Crash cross validating pipeline %s", iteration + 1)
-                    tuner.add(proposed_params, -1000000)
-
-                if pipeline.rank is not None:
-                    tuner.add(proposed_params, 1 - pipeline.rank)
-
-                    cv_end = datetime.utcnow()
-                    cv_time = (cv_end - cv_start).total_seconds()
-                    self.cv_times.append(cv_time)
-                    self.cv_time = np.mean(self.cv_times)
+                if pipeline and (pipeline.rank is not None):
+                    self._tuner_add(proposed_params, 1 - pipeline.rank)
 
                     LOGGER.info("Saving pipeline %s: %s", iteration + 1, pipeline.id)
                     self._save_pipeline(pipeline)
@@ -465,7 +493,10 @@ class PipelineSearcher(object):
                                     self.best_pipeline, self.best_pipeline.rank,
                                     self.best_pipeline.score)
 
-        except (StopSearch, KeyboardInterrupt):
+                else:
+                    self._tuner_add(proposed_params, -1000000)
+
+        except KeyboardInterrupt:
             pass
 
         finally:
@@ -474,14 +505,14 @@ class PipelineSearcher(object):
         if self.current_checkpoint:
             self._checkpoint(final=True)
         elif self.best_pipeline and not checkpoints:
-            self._dump(self.best_pipeline)
+            self._set_for_dump(self.best_pipeline)
 
         if self.best_pipeline:
             LOGGER.info('Best pipeline for problem %s found: %s; rank: %s, score: %s',
-                        problem_id, self.best_pipeline,
+                        self.problem_id, self.best_pipeline,
                         self.best_pipeline.rank, self.best_pipeline.score)
 
         else:
-            LOGGER.info('No pipeline could be found for problem %s', problem_id)
+            LOGGER.info('No pipeline could be found for problem %s', self.problem_id)
 
-        return self.dumped
+        return self._dump_pipelines()
