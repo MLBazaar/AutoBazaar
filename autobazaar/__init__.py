6a364381d351c1ef8fd5cea6fa884765a39b3ac1
Carles Sala <carles@pythiac.com>
2019-11-26 20:21:45 +0100

    Bump version: 0.2.0-dev → 0.2.0

--- autobazaar/__init__.py
+++ autobazaar/__init__.py
@@ -19,7 +19,7 @@ __author__ = 'MIT Data To AI Lab'
 __copyright__ = 'Copyright (c) 2019, MIT Data To AI Lab'
 __email__ = 'dailabmit@gmail.com'
 __license__ = 'MIT'
-__version__ = '0.2.0-dev'
+__version__ = '0.2.0'
 
 
 def _get_commit():
--- setup.py
+++ setup.py
@@ -94,6 +94,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-project/AutoBazaar',
-    version='0.2.0-dev',
+    version='0.2.0',
     zip_safe=False,
 )
