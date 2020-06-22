496f43d7e87e92f1f885ed4a1ee995a64043220a
Carles Sala <carles@pythiac.com>
2020-06-22 13:05:32 +0200

    Bump version: 0.2.1.dev2 → 0.2.1.dev3

--- setup.py
+++ setup.py
@@ -98,6 +98,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-project/AutoBazaar',
-    version='0.2.1.dev2',
+    version='0.2.1.dev3',
     zip_safe=False,
 )
